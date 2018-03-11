513ccb018a503b7bf4137c5f282d26e8d0728120
Nitin Gondule <ni3gondule.com>
2018-03-11 16:18:42 +0530

    initial commit

---
+++ kamnithrms/src/main/java/com/hrms/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hrms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hrms.model.User;
+
+
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+	 User findByEmail(String email);
+}
